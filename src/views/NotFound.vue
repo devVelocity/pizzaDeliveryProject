c42f0057bfe44183f99322099744ba8bd136a737
88147999+exeProgram@users.noreply.github.com <88147999+exeProgram@users.noreply.github.com>
2021-09-05 02:05:35 +0100

    CSS Improvements

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,14 @@
 <template>
-    <div id="orange-background">
-        <h1>404</h1>
-        <h4>Not Found</h4>
-        <h5>Sorry, that page is not available</h5>
-        <router-link class="button-style1" to="/">Go to Homepage</router-link>
-    </div>
+ <div id="orange-background">
+  <transition>
+   <div>
+    <h1>404</h1>
+    <h4>Not Found</h4>
+    <h5>Sorry, that page is not available</h5>
+    <router-link class="button-style1" to="/">Go to Homepage</router-link>
+   </div>
+  </transition>
+ </div>
 </template>
 
 <script>
